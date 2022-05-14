80fe1cb3f4a2a36c2f2d4005c1ab6d380671ec0b
FERNANDAMSS <fernandamachado.s@hotmail.com>
2022-05-14 11:06:42 -0300

    Conectando meu projeto no posters

--- controle-processos-advocaticios/src/main/java/com/fms/ControleProcessosAdvocaticiosApplication.java
+++ controle-processos-advocaticios/src/main/java/com/fms/ControleProcessosAdvocaticiosApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ControleProcessosAdvocaticiosApplication {
-	//FERNANDAMSS
+	//FERNANDAMSS ok
 	//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
 	public static void main(String[] args) {
 		SpringApplication.run(ControleProcessosAdvocaticiosApplication.class, args);
