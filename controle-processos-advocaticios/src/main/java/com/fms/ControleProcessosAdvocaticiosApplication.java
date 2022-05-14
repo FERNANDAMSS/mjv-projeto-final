d8a1b9c33d0eaf40a76fffc0bb5e4deeea663693
FERNANDAMSS <fernandamachado.s@hotmail.com>
2022-05-14 10:37:53 -0300

    teste

--- controle-processos-advocaticios/src/main/java/com/fms/ControleProcessosAdvocaticiosApplication.java
+++ controle-processos-advocaticios/src/main/java/com/fms/ControleProcessosAdvocaticiosApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ControleProcessosAdvocaticiosApplication {
-
+	//FERNANDAMSS
+	//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
 	public static void main(String[] args) {
 		SpringApplication.run(ControleProcessosAdvocaticiosApplication.class, args);
 	}
