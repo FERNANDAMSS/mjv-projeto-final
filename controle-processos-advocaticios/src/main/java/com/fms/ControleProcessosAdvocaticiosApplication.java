d26c75682dd1d4f3ce334e59ee0ebabc5a36070e
FERNANDAMSS <fernandamachado.s@hotmail.com>
2022-05-14 10:34:30 -0300

    testando eclipse

--- controle-processos-advocaticios/src/main/java/com/fms/ControleProcessosAdvocaticiosApplication.java
+++ controle-processos-advocaticios/src/main/java/com/fms/ControleProcessosAdvocaticiosApplication.java
@@ -9,5 +9,6 @@ public class ControleProcessosAdvocaticiosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ControleProcessosAdvocaticiosApplication.class, args);
 	}
+	
 
 }
